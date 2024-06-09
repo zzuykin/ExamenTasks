4c8bdb8306f6e97a44e64308ca9960825abcdb3d
zzuykin <145152148+zzuykin@users.noreply.github.com>
2024-06-09 16:30:57 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,34 @@
+﻿
+IMyClass item = new MyClass();
+item.SayHi();
+item.SaySomeShit();
+item.SayBye();
+
+public interface IMyClass
+{
+    public void SayHi();
+
+    public void SayBye();
+
+    public void SaySomeShit();
+}
+
+
+
+public class MyClass : IMyClass
+{
+    public void SayHi()
+    {
+        Console.WriteLine("Hi");
+    }
+
+    public void SayBye()
+    {
+        Console.WriteLine("Bye!");
+    }
+
+    public void SaySomeShit()
+    {
+        Console.WriteLine("You are shit");
+    }
+}
