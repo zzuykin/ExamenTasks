98f18f99d81c91f18cdc12d2f0be673c684ee0bb
zzuykin <145152148+zzuykin@users.noreply.github.com>
2024-06-09 16:38:44 +0300

    Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,25 @@
+﻿
+var pub = new Publisher();
+pub.MyEvent += MyFunc;
+pub.RaseEvent(25);
+
+void MyFunc(int x)
+{
+    Console.WriteLine($"My num is {x}");
+}
+public class Publisher
+{
+    public delegate void PubDelegate(int x);
+
+    public event PubDelegate MyEvent;
+
+    public void RaseEvent(int x)
+    {
+        if (MyEvent != null)
+        {
+            MyEvent(x);
+        }
+    }
+}
+
+
