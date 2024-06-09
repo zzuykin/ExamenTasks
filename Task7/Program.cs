8117f5972245db798ef5f8ff483342e9c1eb7643
zzuykin <145152148+zzuykin@users.noreply.github.com>
2024-06-09 16:06:56 +0300

    8 task

---
+++ Task7/Program.cs
@@ -0,0 +1,12 @@
+﻿var item = new MyClass<string>();
+item.WhatType();
+
+public class MyClass<T>
+{
+    public static T? type;
+
+    public void WhatType()
+    {
+        Console.WriteLine(typeof(T));
+    }
+}
---
+++ Task8/Program.cs
@@ -0,0 +1,35 @@
+﻿var item = new MyCLass<MyBaseClass>(new MyBaseClass());
+var item2 = new MyCLass<MyOtherClass>(new MyOtherClass());
+
+//var item3 = new MyCLass<int>(); WRONG
+
+
+public class MyCLass<T> where T: MyBaseClass
+{
+    public MyCLass(T value)
+    {
+        Value = value;
+    }
+
+    public T Value { get; set; }
+
+}
+
+public class MyBaseClass
+{
+    private string name { get; set; }
+
+    private int age { get; set; }
+
+    public MyBaseClass(string Name, int Age)
+    {
+        name = Name;
+        age = Age;
+    }
+    public MyBaseClass() { }
+}
+
+public class MyOtherClass : MyBaseClass
+{
+
+}
