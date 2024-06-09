77a1c9989ac2fe01daff5ac595053e85c71ac03d
zzuykin <145152148+zzuykin@users.noreply.github.com>
2024-06-09 17:19:20 +0300

    Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+var context = new MyDbContext();
+var testTable = new MyTable() {Name = "Nikita" };
+
+context.MyTables.Add(testTable);
+context.SaveChanges();
+
+public class MyTable
+{
+    [Key]
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+}
+
+public class MyDbContext : DbContext
+{
+    public DbSet<MyTable> MyTables { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TestDb;Trusted_Connection=True;");
+    }
+}
+
