3864e26b1b8b7cf3e301b6324e5fbff20705d98c
zzuykin <145152148+zzuykin@users.noreply.github.com>
2024-06-10 12:55:33 +0300

    Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+
+var rand = new Random();
+int X = 500;
+int countOfThreads = 10;
+var stop = new Object();
+
+long[] times = new long[countOfThreads];
+Thread[] myThreads = new Thread[countOfThreads];
+
+for(int i = 0; i < countOfThreads; i++)
+{
+    int numThread = i;
+    myThreads[i] = new Thread(() =>
+    {
+        GenerateArray(X, times, numThread);
+    });
+}
+
+foreach(var thread in myThreads)
+{
+    thread.Start();
+}
+
+foreach (var thread in myThreads)
+{
+    thread.Join();
+}
+
+long[] filterTimes = times.Where(t => t > 0).ToArray();
+
+Console.WriteLine($"Минимальное время ожидания {filterTimes.Min()} ms");
+Console.WriteLine($"Среднее время ожидания {filterTimes.Average()} ms");
+Console.WriteLine($"Максимальное время ожидания {filterTimes.Max()}  ms");
+
+void GenerateArray(int x, long[] times, int threadIndex)
+{
+    int size = rand.Next(10_000_000, 15_000_001);
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(0, 1001);
+    }
+
+    Array.Sort(array);
+
+    Stopwatch time = Stopwatch.StartNew();
+
+    lock (stop)
+    {
+        times[threadIndex] = time.ElapsedMilliseconds;
+        int count = array.Count(n => n == X);
+
+        Console.WriteLine($"Поток {threadIndex} найдено значений {count}");
+    }
+}
