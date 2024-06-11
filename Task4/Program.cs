9bf4943a4ff23fc96970510837241f96decbe557
zzuykin <145152148+zzuykin@users.noreply.github.com>
2024-06-11 17:57:55 +0300

    Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int X = Convert.ToInt32(Console.ReadLine());
+var rand = new Random();
+Base[] array1 = new Base[X];
+BaseChild[] array2 = new BaseChild[X];
+Predicate<int> isTrue = x => x == 1;
+
+for(int i = 0; i < X; i++)
+{
+    array1[i] = new Base("Base", isTrue(rand.Next(0, 2)));
+    array2[i] = new BaseChild("Child", isTrue(rand.Next(0, 2)));
+}
+
+if(array1.Count(x => x._bool == true) > array2.Count(x => x._bool == true))
+{
+    Console.WriteLine("BASE!");
+}
+else
+{
+    Console.WriteLine("CHILD!");
+}
+
+public class Base
+{
+    public string _string;
+    public bool _bool;
+
+    public Base(string @string, bool @bool)
+    {
+        _string = @string;
+        _bool = @bool;
+    }
+}
+
+public class BaseChild : Base
+{
+    public BaseChild(string @string, bool @bool) : base(@string, @bool) { }
+}
